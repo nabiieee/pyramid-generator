5f86a9564c1b30d4f7acd554b3f38499dd50b253
sucharita13 <sucharita.madan13@gmail.com>
2025-02-15 19:47:20 +0530

    Pyramid Generator

---
+++ main.js
@@ -0,0 +1,32 @@
+//chapter 1
+//question1
+let a = "Sucharita";
+let b = 13;
+console.log(a+b);
+// The answer will 'Sucharita13' since the string concatenates it.
+
+//question2
+console(typeof (a+b));
+//The answer will be string since the it got concatenated so the final datatype will be string.
+
+//Question3
+const c = { name: "sucharita",
+rollNo: 13};
+ c = 13;
+//The a will not be redeclared since it declared as const hence, the values can't be changed.
+
+//Question4
+c['friend']="Bharti";
+//The keys can be changed in the const i.e we can add the values and modify the values in the const declared object but not the object itself.
+
+//Question5
+const dictionary = {
+    bts: "bangtanseonyeontan",
+    army:"fandom",
+    festa:"anniversary celebration event"
+}
+
+console.log(dictionary.bts);
+
+//Once the program runs then the meaning of bts(full form) will be printed.
+
